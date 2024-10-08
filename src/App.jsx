833245031f05ec31461794c00bfa6abca12cd119
duycoding <128799277+duycoding@users.noreply.github.com>
2024-10-08 13:56:39 +0700

    step1

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import axios from "./utils/axios.customize"
+import { useEffect } from "react"
+
+function App() {
+  useEffect(() =>  {
+    const fetchHelloWorld = async () => {
+      const res = await axios.get(`/v1/api/`)
+      console.log("Check res: ", res)
+    }
+    fetchHelloWorld()
+  }, [])
+  return (
+    <>
+      Hello World
+    </>
+  )
+}
+
+export default App
